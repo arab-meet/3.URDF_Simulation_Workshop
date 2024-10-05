38e5923e3b7dfeedffb780c2ed4b9766234c1fb6
Rawda-Abdelkhalek <rawdaabdelkhalk@gmail.com>
2024-10-05 19:55:12 +0300

    update TF

--- C. TF/static_dynamic_pkg/src/tf_static_listener.cpp
+++ C. TF/static_dynamic_pkg/src/tf_static_listener.cpp
@@ -3,7 +3,7 @@
 #include <geometry_msgs/TransformStamped.h>
 
 int main(int argc, char** argv){
-  ros::init(argc, argv, "my_tf2_listener");
+  ros::init(argc, argv, "tf2_listener");
 
   ros::NodeHandle node;
   tf2_ros::Buffer tfBuffer;
@@ -13,14 +13,19 @@ int main(int argc, char** argv){
   while (node.ok()){
     geometry_msgs::TransformStamped transformStamped;
     try{
-      transformStamped = tfBuffer.lookupTransform("turtle2", "turtle1",
-                               ros::Time(0));
+      transformStamped = tfBuffer.lookupTransform("base_link", "laser",
+                               ros::Time::now());
     }
     catch (tf2::TransformException &ex) {
       ROS_WARN("%s",ex.what());
       ros::Duration(1.0).sleep();
       continue;
     }
+    // Do something with the transformStamped
+    ROS_INFO("Received transform from base_link to laser: %f, %f, %f",
+             transformStamped.transform.translation.x,
+             transformStamped.transform.translation.y,
+             transformStamped.transform.translation.z);
     rate.sleep();
   }
   return 0;
